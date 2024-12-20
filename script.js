14a1c25fab75cadc183472d1307798777e4b161b
dsaikiran01 <dsaikiran559@gmail.com>
2024-12-20 23:35:48 +0530

    added seo, smooth transitions, bug fixes

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
+const navMenu = document.querySelector(".nav-menu");
 const navLinks = document.querySelectorAll(".nav-link");
+const logo = document.querySelector('.logo-a');
 
 const roles = ["web developer", "software engineer", "cloud enthusiast", "open-source contributor"];
 let currentIndex = 0;
-
-// Toggle hamburger menu state
-function toggleHamburger() {
-    const hamburger = document.querySelector('.hamburger');
-    hamburger.classList.toggle('active');
-    const menu = document.querySelector('.nav-menu');
-    menu.classList.toggle('active'); // Toggle menu visibility on click
-}
 
 function changeRole() {
     const roleElement = document.getElementById("role");
@@ -19,12 +12,59 @@ function changeRole() {
     roleElement.textContent = roles[currentIndex];
 }
 
+// for smooth scrolling of navlinks
+// Get all the anchor links from the navbar
+// const links = document.querySelectorAll('div a'); // capturing every <a> including show all
+const links = document.querySelectorAll('.nav-link');
+
+links.forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        // Get the target section from the href attribute
+        const targetId = link.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        // Scroll to the target section with smooth scrolling
+        targetSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
+
+// when clicked on logo
+logo.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // Get the target section from the href attribute
+    const targetId = logo.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+
+    // Scroll to the target section with smooth scrolling
+    targetSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+});
+
+// Toggle hamburger menu state
+function toggleHamburger() {
+    const hamburger = document.getElementById('hamburger');
+    hamburger.classList.toggle('active');
+    const menu = document.querySelector('.nav-menu');
+    menu.classList.toggle('active'); // Toggle menu visibility on click
+}
+
 const mobileMenu = () => {
     navMenu.classList.toggle("display");
+    // hamburger.classList.toggle("active");
 }
 
 const closeMenu = () => {
     navMenu.classList.remove("display");
+    navMenu.classList.remove("active");
+    hamburger.classList.remove("active");
 }
 
 // changing roles on About
